7e3241655c1df2c0dd25ceb93184408f3fc2afaa
TimS <92215345+Tim-Adcoder@users.noreply.github.com>
2022-07-10 00:01:24 +0930

    add generate-site code

---
+++ index.js
@@ -0,0 +1,19 @@
+"what is the team manger's name?"
+"what is the team manager's ID?"
+"what is the team manager's email?"
+"what is the team manager's office number?"
+
+"which type of team member would you like to add?" 
+"Engineer" 
+"Intern" 
+"I dont want to add any more team members"
+
+"what is the team engineer's name?"
+"what is the team engineer's ID?"
+"what is the team engineer's email?"
+"what is the team engineer's Github username?"
+
+"what is the team intern's name?"
+"what is the team intern's ID?"
+"what is the team intern's email?"
+"what is the team intern's school?"
