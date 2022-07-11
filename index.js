44fdd18cbdf2f3fa72104f3686b5d7de352e6861
TimS <92215345+Tim-Adcoder@users.noreply.github.com>
2022-07-11 16:33:03 +0930

    debugging

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const inquirer = require('inquirer');
+import inquirer from 'inquirer';
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
